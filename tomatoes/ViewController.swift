344f823a9eefee97a0547f58c410b5f142fbb13c
Yuan Zhang <yzhang29@keptputside-lm.corp.yahoo.com>
2014-09-09 18:44:35 -0700

    Initial Commit

---
+++ tomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tomatoes
+//
+//  Created by Yuan Zhang on 9/9/14.
+//  Copyright (c) 2014 Yuan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
